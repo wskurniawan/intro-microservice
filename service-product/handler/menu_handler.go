3e944d19e1e6540cb6a7533199be41ff885ff3fa
Wisnu Kurniawan <kurniawan.wisnu23@gmail.com>
2020-10-14 22:17:08 +0700

    using utils wrapper

--- service-product/handler/menu_handler.go
+++ service-product/handler/menu_handler.go
@@ -1,21 +1,11 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
+	"github.com/wskurniawan/intro-microservice/utils"
 	"net/http"
 )
 
 // AddMenuHandler handle add menu
 func AddMenuHandler(w http.ResponseWriter, r *http.Request) {
-	response, err := json.Marshal(map[string]interface{}{
-		"success": true,
-	})
-
-	if err != nil {
-		fmt.Printf("Failed generate response %s", err)
-		return
-	}
-
-	w.Write(response)
+	utils.WrapAPISuccess(w, r, "success", 200)
 }
